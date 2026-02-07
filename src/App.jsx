b2a4803cced1f0b3eaa8cd9f3f3680cb7c94decb
Aditya Kumar Soni <soniadityakumar651@gmail.com>
2026-02-08 01:06:30 +0530

    Review

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import HomePage from "./pages/Home";
 import AuthPage from "./pages/Auth";
 import CheckoutPage from "./pages/Checkout";
 import Navbar from "./components/Navbar";
+import AuthProvider from "./context/AuthContext";
 export default function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
